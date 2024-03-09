d80cda7e0d883790e4a02b66a898467838ccf4d0
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-03-09 19:01:58 +0000

    Todo_app

--- project_name/Todo_app/models.py
+++ project_name/Todo_app/models.py
@@ -8,6 +8,6 @@ from django.db import models
 class Task(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
+    completed = models.BooleanField(default=False)
     def __str__(self):
         return self.description
